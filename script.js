0702468f3d1d1a1a4a7a4aa8a981a1e3c0d2426b
adodjoaristide9-lab <you@example.com>
2026-02-27 21:52:46 +0100

    mise à jour de html css et js par Aristide

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-voici javascript
+// Ce code sert à récupérer l'icône du menu
+const menuIcon = document.querySelector('.menu-icon');
+
+// Ce code sert à récupérer la barre de navigation
+const navbar = document.getElementById('navbar');
+
+// Ce code dit : quand on clique sur l'icône, on ouvre ou ferme le menu
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('open');
+});
